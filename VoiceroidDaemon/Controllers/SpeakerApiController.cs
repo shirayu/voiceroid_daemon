1aeb7355b730fb96264150b0576529fec952b607
yuta-h <yuta-h@YUKARI>
2020-03-20 12:52:54 +0900

    Added api/set/speaker

--- VoiceroidDaemon/Controllers/SpeakerApiController.cs
+++ VoiceroidDaemon/Controllers/SpeakerApiController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Aitalk;
 using Microsoft.AspNetCore.Mvc;
+using VoiceroidDaemon.Models;
 
 namespace VoiceroidDaemon.Controllers
 {
@@ -25,7 +26,7 @@ namespace VoiceroidDaemon.Controllers
     public class GetSpeakersController : ControllerBase
     {
         /// <summary>
-        /// 話者リストを取得する
+        /// 話者リストを取得する．全ライブラリをロードするので時間がかかる．
         /// </summary>
         /// <returns></returns>
         public IActionResult GetSpeakers()
@@ -39,9 +40,51 @@ namespace VoiceroidDaemon.Controllers
                 var voice_names = AitalkWrapper.Parameter.VoiceNames;
                 libname2speaker.Add(libname, voice_names);
             }
-            Setting.ApplySpeakerSetting(Setting.Speaker);
+            _ = Setting.ApplySpeakerSetting(Setting.Speaker);
             Setting.Unlock();
             return Ok(libname2speaker);
+        }
+    }
+
+    [Route("api/set/speaker")]
+    [ApiController]
+    public class SetSpeakerController : ControllerBase
+    {
+        /// <summary>
+        /// 現在の話者情報を更新する
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult SpeakerSetting(SpeakerSettingModel speaker_setting)
+        {
+            if (speaker_setting != null)
+            {
+                string error_message = null;
+                var saved = false;
+                Setting.Lock();
+                try
+                {
+                    error_message = Setting.ApplySpeakerSetting(speaker_setting);
+                    saved = Setting.Save();
+                }
+                finally
+                {
+                    Setting.Unlock();
+                }
+                if (saved == false)
+                {
+                    return BadRequest("Failed to save");
+                }
+                else if (error_message != null)
+                {
+                    return BadRequest($"Saved but {error_message}");
+                }
+                else
+                {
+                    return Ok("OK");
+                }
+            }
+            return NotFound();
         }
     }
 }
